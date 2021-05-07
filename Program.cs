af5a02293fbda698dd848609198f4750a85295c2
Dmitri Zigulev <82953897+Dim43a@users.noreply.github.com>
2021-05-07 18:30:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,89 +8,39 @@ namespace Cube
     {
         static void Main(string[] args)
         {
+            
             string s = "abcd";
-            string t = MixAndShow(s);
-            AddedChar(s,t);
-            
 
 
-            //*Using LINQ
-            //Random rnd = new Random();
-            //string str = "abcd";
-            //char randomChar = (char)rnd.Next('a', 'z');
-            //str = str + randomChar;
-            //string rand = new string(str.ToCharArray().OrderBy(s => (rnd.Next(2) % 2) == 0).ToArray()); 
-            //Console.WriteLine(str);
-            //Console.WriteLine(rand);
-            
-            //WORKING Char finder without classes
-            //string s = "a";
-            //string t = "aa";
-            //char[] s1 = s.ToCharArray();
-            //char[] t1 = t.ToCharArray();
-            //var tempS1 = s1.ToList();
-            //var tempT1 = t1.ToList();
 
-            //for (int i = 0; i < tempS1.Count; i++)
-            //{
-            //    for (int j = 0; j < tempT1.Count; j++)
-            //    {
-            //        if (tempS1[i] == tempT1[j])
-            //        {
-            //            tempS1.RemoveAt(i);
-            //            tempT1.RemoveAt(j);
-            //        }
-            //    }
-            //    i--;
-            //}
-
-            //foreach (var item in tempT1)
-            //{
-            //    Console.WriteLine(item);
-            //}
-           
+            string t = Shuffler(s);
+            AddedLetter(s, t);
 
 
             Console.ReadKey();
         }
-        public static string MixAndShow(string s)
+
+        public static string Shuffler(string s )
         {
             Random rnd = new Random();
             char randomChar = (char)rnd.Next('a', 'z');
-            char[] sArray = s.ToCharArray();
-            char[] randomArray = new char[sArray.Length + 1];
-            randomArray[sArray.Length] = randomChar;
-            int randomnumber;
-            string output = "";
+            Console.WriteLine("s : {0}", s);
+            s += randomChar;
 
+            string rand = new string(s.ToCharArray().OrderBy(s => (rnd.Next(2) % 2) == 0).ToArray()); ////Using LINQ
+            Console.WriteLine("t : {0}",rand);
 
-            for (int i = sArray.Length; i >= 1; i--)
-            {
-                randomnumber = rnd.Next(1, i + 1) - 1;
-                randomArray[i - 1] = sArray[randomnumber];
-                sArray[randomnumber] = sArray[i - 1];
-            }
-
-            Console.WriteLine("s = {0} ", s);
-            Console.Write("t = ");
-
-            foreach (char o in randomArray)
-            {
-                Console.Write(o) ;
-            }
-
-            return output;
+            return rand;
 
         }
-
-        public static string AddedChar ( string s, string t)
+        public static string AddedLetter(string s, string t)
         {
             char[] s1 = s.ToCharArray();
             char[] t1 = t.ToCharArray();
             var tempS1 = s1.ToList();
             var tempT1 = t1.ToList();
-            string output = "";
 
+            string result = "";
 
             for (int i = 0; i < tempS1.Count; i++)
             {
@@ -107,11 +57,11 @@ namespace Cube
 
             foreach (var item in tempT1)
             {
-                Console.WriteLine(item);
+                Console.WriteLine( item);
             }
 
-            return output;
-        }
+            return result;
+        }    
     }
 }
 
