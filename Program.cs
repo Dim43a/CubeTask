265e41691dd1400b33ec4ac87f7f722055342e69
Dmitri Zigulev <82953897+Dim43a@users.noreply.github.com>
2021-05-10 08:12:58 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,12 +12,41 @@ namespace Cube
             string s = "abcd";
             Console.WriteLine("s : {0}", s);
 
-
-
             string t = Shuffler(s);
             Console.WriteLine("t : {0}", t);
             
             AddedLetter(s, t);
+            
+            
+            
+            string s1="";
+            Console.WriteLine("s : {0}", s1);
+            
+            string t1="y";
+            Console.WriteLine("t : {0}", t1);
+            
+            AddedLetter(s1,t1);  //y
+            
+            
+            
+            string s2="a";
+            Console.WriteLine("s : {0}", s2);
+            
+            string t2="aa";
+            Console.WriteLine("t : {0}", t2);
+            
+            AddedLetter(s2,t2); //a
+            
+            
+            
+            string s3="ae";
+            Console.WriteLine("s : {0}", s3);
+            
+            string t3="aea";
+            Console.WriteLine("t : {0}", t3);
+            
+            AddedLetter(s3,t3); //a
+            
 
 
             Console.ReadKey();
@@ -29,9 +58,9 @@ namespace Cube
             char randomChar = (char)rnd.Next('a', 'z');
             s += randomChar;
 
-            string rand = new string(s.ToCharArray().OrderBy(s => (rnd.Next(2) % 2) == 0).ToArray()); ////Using LINQ
+            string randomString = new string(s.ToCharArray().OrderBy(s => (rnd.Next(2) % 2) == 0).ToArray()); ////Using LINQ
 
-            return rand;
+            return randomString;
 
         }
         public static string AddedLetter(string s, string t)
@@ -58,7 +87,7 @@ namespace Cube
 
             foreach (var item in tempT1)
             {
-                Console.WriteLine( item);
+                Console.WriteLine(item);
             }
 
             return result;
